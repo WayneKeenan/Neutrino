813a15619096bc89f9d047fa6a23f9d81336278b
Gareth Noyce <garethnoyce@triple-eh.net>
2016-06-20 16:49:42 +0300

    Fixed assert being out of scope in GLUtils

--- source/framework/GLUtils.cpp
+++ source/framework/GLUtils.cpp
@@ -3,106 +3,108 @@
 
 namespace Neutrino {
 
-        namespace GLUtils {
+    namespace GLUtils {
 
-                static glm::mat4 mCameraMatrix;
-                static glm::mat4 mModelViewMatrix;
+        static glm::mat4 mCameraMatrix;
+        static glm::mat4 mModelViewMatrix;
 
-                float* GetCameraMatrix()
-                {
-                        return &mCameraMatrix[0][0];
-                }
-
-                void SetViewport(const int iScreenWidth, const int iScreenHeight)
-                {
-                        LOG_WARNING("GLUtils::SetViewport working with fixed aspect ratio in ortho ProjectionMatrix calc...");
-                        glViewport(0, 0, iScreenWidth, iScreenHeight);       
-                        glEnable(GL_DEPTH_TEST);
-                        glEnable(GL_BLEND);
-                        glActiveTexture(GL_TEXTURE0);
-                        glClearColor( 0.05f, 0.15f, 0.25f, 1.0f );
-                        glClearDepth(1.0f);
-                        glDepthFunc(GL_LEQUAL);
-
-                        glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-                        GL_ERROR;
-                        
-                        glm::mat4 mRotationMatrix = glm::rotate(glm::mat4(), 0.0f, glm::vec3(0.0f, 0.0f, 0.0f));
-                        glm::mat4 mTranslationMatrix = glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, 0.0f));
-                        glm::mat4 mProjectionMatrix = glm::ortho(0.0f, (float)((float)iScreenWidth/(float)iScreenHeight), 0.0f, 1.0f, 0.1f, 10.0f );
-
-                        mModelViewMatrix = mTranslationMatrix * mRotationMatrix;
-                        mCameraMatrix = mProjectionMatrix * mModelViewMatrix;
-                }
-
-
-                void CreateVBO()
-                {
-                        GLuint iVBO_ID;
-
-                        glGenBuffers(1, &iVBO_ID); 
-                        GL_ERROR;
-                        glBindBuffer(GL_ARRAY_BUFFER, iVBO_ID); 
-                        GL_ERROR;
-                        glBufferData(GL_ARRAY_BUFFER, (sizeof(Vertex_t) * 6) * s_iMaxSprites, NULL, GL_DYNAMIC_DRAW  );
-                        GL_ERROR;
-                }
-
-
-                void DeleteVBO( GLuint iVBO_ID )
-                {
-                        glDeleteBuffers( 1, &iVBO_ID );
-                        GL_ERROR;
-                }
-
-                void TestRender()
-                {
-                        glBegin(GL_TRIANGLES);
-                        glVertex3f(0.0f, 2.0f, 1.0f);
-                        glVertex3f(-2.0f, -2.0f, 1.0f);
-                        glVertex3f(2.0f, -2.0f, 1.0f);
-                        glEnd();
-                }
-
-                bool LogGlError(const char *pFile, int iLine) 
-                {
-                        bool bReturnVal = false;
-                        GLenum err (glGetError());
-
-                        while(err!=GL_NO_ERROR) 
-                        {
-                                switch(err) 
-                                {
-                                        case GL_INVALID_OPERATION:      
-                                                LOG_ERROR("%s, %d: INVALID_OPERATION", pFile, iLine);   
-                                                bReturnVal = true;   
-                                                break;
-
-                                        case GL_INVALID_ENUM:           
-                                                LOG_ERROR("%s, %d: INVALID_ENUM", pFile, iLine);           
-                                                bReturnVal = true;   
-                                                break;
-
-                                        case GL_INVALID_VALUE:          
-                                                LOG_ERROR("%s, %d: INVALID_VALUE", pFile, iLine);          
-                                                bReturnVal = true;   
-                                                break;
-
-                                        case GL_OUT_OF_MEMORY:          
-                                                LOG_ERROR("%s, %d: OUT_OF_MEMORY", pFile, iLine);
-                                                bReturnVal = true;   
-                                                break;
-
-                                        case GL_INVALID_FRAMEBUFFER_OPERATION:  
-                                                LOG_ERROR("%s, %d: INVALID_FRAMEBUFFER_OPERATION", pFile, iLine);  
-                                                bReturnVal = true;   
-                                                break;
-
-                                }
-                                err=glGetError();
-                        }
-
-                        return bReturnVal;
-                }
+        float* GetCameraMatrix()
+        {
+            return &mCameraMatrix[0][0];
         }
+
+
+
+        void SetViewport(const int iScreenWidth, const int iScreenHeight)
+        {
+            LOG_WARNING("GLUtils::SetViewport working with fixed aspect ratio in ortho ProjectionMatrix calc...");
+            glViewport(0, 0, iScreenWidth, iScreenHeight);       
+            glEnable(GL_DEPTH_TEST);
+            glEnable(GL_BLEND);
+            glActiveTexture(GL_TEXTURE0);
+            glClearColor( 0.05f, 0.15f, 0.25f, 1.0f );
+            glClearDepth(1.0f);
+            glDepthFunc(GL_LEQUAL);
+
+            glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            GL_ERROR;
+
+            glm::mat4 mRotationMatrix = glm::rotate(glm::mat4(), 0.0f, glm::vec3(0.0f, 0.0f, 0.0f));
+            glm::mat4 mTranslationMatrix = glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, 0.0f));
+            glm::mat4 mProjectionMatrix = glm::ortho(0.0f, (float)((float)iScreenWidth/(float)iScreenHeight), 0.0f, 1.0f, 0.1f, 10.0f );
+
+            mModelViewMatrix = mTranslationMatrix * mRotationMatrix;
+            mCameraMatrix = mProjectionMatrix * mModelViewMatrix;
+        }
+
+
+        void CreateVBO()
+        {
+            GLuint iVBO_ID;
+            glGenBuffers(1, &iVBO_ID); 
+            ASSERT_GL_ERROR;
+            glBindBuffer(GL_ARRAY_BUFFER, iVBO_ID); 
+            ASSERT_GL_ERROR;
+            glBufferData(GL_ARRAY_BUFFER, (sizeof(Vertex_t) * 6) * s_iMaxSprites, NULL, GL_DYNAMIC_DRAW  );
+            ASSERT_GL_ERROR;
+        }
+
+
+        void DeleteVBO( GLuint iVBO_ID )
+        {
+            glDeleteBuffers( 1, &iVBO_ID );
+            GL_ERROR;
+        }
+
+
+        void TestRender()
+        {
+            glBegin(GL_TRIANGLES);
+            glVertex3f(0.0f, 2.0f, 1.0f);
+            glVertex3f(-2.0f, -2.0f, 1.0f);
+            glVertex3f(2.0f, -2.0f, 1.0f);
+            glEnd();
+        }
+
+
+        bool LogGlError(const char *pFile, int iLine) 
+        {
+            bool bReturnVal = false;
+            GLenum err (glGetError());
+
+            while(err!=GL_NO_ERROR) 
+            {
+                switch(err) 
+                {
+                    case GL_INVALID_OPERATION:      
+                        LOG_ERROR("%s, %d: INVALID_OPERATION", pFile, iLine);   
+                        bReturnVal = true;   
+                        break;
+
+                    case GL_INVALID_ENUM:           
+                        LOG_ERROR("%s, %d: INVALID_ENUM", pFile, iLine);           
+                        bReturnVal = true;   
+                        break;
+
+                    case GL_INVALID_VALUE:          
+                        LOG_ERROR("%s, %d: INVALID_VALUE", pFile, iLine);          
+                        bReturnVal = true;   
+                        break;
+
+                    case GL_OUT_OF_MEMORY:          
+                        LOG_ERROR("%s, %d: OUT_OF_MEMORY", pFile, iLine);
+                        bReturnVal = true;   
+                        break;
+
+                    case GL_INVALID_FRAMEBUFFER_OPERATION:  
+                        LOG_ERROR("%s, %d: INVALID_FRAMEBUFFER_OPERATION", pFile, iLine);  
+                        bReturnVal = true;   
+                        break;
+
+                }
+                err=glGetError();
+            }
+            return bReturnVal;
+        }
+    }
 }
--- source/framework/GLUtils.h
+++ source/framework/GLUtils.h
@@ -4,15 +4,16 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/mat4x4.hpp>
 #include "Types.h"
+#include "Assert.h"
 
 // Macros for glError Wrapper
 //
 #if defined(DEBUG)
-	#define GL_ERROR do{GLUtils::LogGlError(__FILE__,__LINE__);}while(0)
-	#define ASSERT_GL_ERROR do{ASSERT(!GLUtils::LogGlError(__FILE__,__LINE__));}while(0)
+#define GL_ERROR do{GLUtils::LogGlError(__FILE__,__LINE__);}while(0)
+#define ASSERT_GL_ERROR do{ASSERT(!GLUtils::LogGlError(__FILE__,__LINE__));}while(0)
 #else
-	#define GL_ERROR
-	#define ASSERT_GL_ERROR
+#define GL_ERROR
+#define ASSERT_GL_ERROR
 #endif
 
 namespace Neutrino 
@@ -20,29 +21,36 @@ namespace Neutrino
 
 	namespace GLUtils 
 	{
-
-		// Temporary MAX_SPRITES setting
-		static const uint16 s_iMaxSprites = 4096*2;
-
 		typedef struct Vertex_t
 		{
-			glm::vec4 _colours; 
+			uint32_t _colour; 
 			glm::vec2 _uv;
 			glm::vec3 _position;
 		} Vertex_t; 
 
+		static const uint16 s_iMaxSprites = 4096*2;		// Temporary, need to find a balance of the amount we're copying vs number needed.
 		static const uint16 s_iSizeOfVertext = sizeof(Vertex_t);
 
 
+
+		
+		// Temporary
 		void TestRender();
 
 		// SetViewport
 		//		Attempts to setup the OGL viewport for given dimensions and calcs the associated matrices
 		void SetViewport(const int iScreenWidth, const int iScreenHeight);
 
+		// LogGlError
+		// 	 Wrapper to chcek glError and LOG_ERR to log file in a clean way. 
+		// 	 Use the GL_ERROR / ASSERT_GL_ERROR macros to use this in code...
+		bool LogGlError(const char *pFile, int iLine);
+
+		// CreateVBO
+		//		TODO:
+		//			Return a struct of 3 VBOs (triple buffering) of s_iMaxSprites in size
 		void CreateVBO();
 
-		bool LogGlError(const char *pFile, int iLine);
 
 		// GetCameraMatrix
 		// 		Returns address of the camera matrix for current viewport
@@ -50,6 +58,7 @@ namespace Neutrino
 
 
 		// DeleteVBO
+		// 		Safely cleans up any created VBOs, with error checking.
 		void DeleteVBO( GLuint iVBO_ID  );
 	};
 }

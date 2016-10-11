fec5d84907b325ead15fb01fd6e9876ba09dfd42
Gareth Noyce <garethnoyce@triple-eh.net>
2016-10-11 22:20:48 +0300

    Debug Overlay. FPS and Delta, with graphs

--- source/framework/Debug.cpp
+++ source/framework/Debug.cpp
@@ -1,11 +1,21 @@
 #include "Debug.h"
 #include "imgui/imgui.h"
 #include "Memory.h"
+#include "Time.h"
+#include "Input.h"
+#include <string.h>
+#include "Input.h"
+
 
 namespace Neutrino
 {
 	static bool s_bDebugOverlayActive = true;
 	static const ImVec2* s_pOverlayPosition = NEWX ImVec2(50.0f, 50.0f);
+
+	static CFrameRate s_FPSSampler;
+
+	static char sFPS_Text[32] = {0};
+	static char sDELTA_Text[32] = {0};
 
 	void ToggleDebugOverlay()
 	{
@@ -14,12 +24,20 @@ namespace Neutrino
 
 	void DrawDebugOverlay()
 	{
+		//float fFPS = (float)floorf( 1.0f / (float)fmax(fEPSILON, (float)fAvgFPS )+0.5f );
+		s_FPSSampler.Tick(GetMSDelta());
+
 		if (s_bDebugOverlayActive)
         {
+        	snprintf(sFPS_Text, 32,   "FPS: %.2f [%.2f]", s_FPSSampler.GetAverageFrameRate(), s_FPSSampler.GetLatestFrameRate() );
+        	snprintf(sDELTA_Text, 32, "DELTA: [%.2f]", s_FPSSampler.GetAverageDeltaTime());
+
         	ImGui::SetNextWindowPos(*s_pOverlayPosition, ImGuiSetCond_FirstUseEver);
-            ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
-            ImGui::Begin("Debug Overlay", &s_bDebugOverlayActive, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-            ImGui::Text("Hello");
+            ImGui::SetNextWindowSize(ImVec2(300,120), ImGuiSetCond_FirstUseEver);
+            ImGui::Begin("Neutrino Debug", &s_bDebugOverlayActive);
+            ImGui::Text("Performance:");
+            ImGui::PlotLines(sDELTA_Text, s_FPSSampler.GetSamples(), (int)s_FPSSampler.GetSampleSize(), (int)s_FPSSampler.GetSampleOffset(), "", 0.0f, 0.16f, ImVec2(150,32));
+            ImGui::PlotLines(sFPS_Text, s_FPSSampler.GetFPS(), (int)s_FPSSampler.GetSampleSize(), (int)s_FPSSampler.GetSampleOffset(), "", 30.0f, 60.0f, ImVec2(150,32));
             ImGui::End();
         }
 	}	
--- source/framework/Debug.h
+++ source/framework/Debug.h
@@ -1,8 +1,130 @@
 #pragma once
+#include "Types.h"
+#include <array>
+#include "Assert.h"
+
 
 namespace Neutrino
 {
-
+	// ToggleDebugOverlay()
+	// 		Turn the overlay on / off. No formal key assigned to this, although SDL wrapper currently checks for F1	
 	void ToggleDebugOverlay();
+
+	//	DrawDebugOverlay()
+	// 		Creates and new ImGUI window and does all the processing necessary for our debug output. 
 	void DrawDebugOverlay();
+
+
+	// ------------------------------------------------------------------------------------------------------
+
+	// 
+	// Nicked from Barry C. / Garez
+	// 
+	// Frame rate monitor. This is used to keep a frame-rate current based on the last
+	// 'N' samples of time deltas.
+	template<typename Sample, size_t SAMPLES>
+	class CFrameRateT
+	{
+	public:
+		CFrameRateT(): m_samples(0), m_sum(0)
+		{
+			ASSERT(m_sample.size() > 0);
+		}
+
+		void Tick(Sample sample)
+		{
+			ASSERT(sample > 0);
+			if (m_samples < m_sample.size())
+			{
+				// Still reading the first 'm_sample.size()' numbers
+				m_sample[m_samples++] = sample;
+				m_sum += sample;
+				m_fps[m_samples] = (1 / GetLatestDeltaTime());
+			}
+			else
+			{
+				// Loop subsequent samples around [m_sample.size()..(m_sample.size()*2-1)]
+				Sample& slot = m_sample[m_samples % m_sample.size()];
+				m_sum += sample - slot;
+				slot = sample;
+				m_fps[m_samples % m_fps.size()] = (1 / GetLatestDeltaTime());
+				if (++m_samples >= (m_sample.size() * 2))
+				{
+					m_samples = m_sample.size();
+				}
+			}
+			ASSERT(m_sum > 0);
+		}
+		Sample GetLatestDeltaTime() const
+		{
+			// Determine the latest sample, if any
+			if (m_samples > 0)
+			{
+				size_t index = (m_samples + m_sample.size() - 1) % m_sample.size();
+				return m_sample[index];
+			}
+			return 0;
+		}
+
+		Sample GetLatestFrameRate() const
+		{
+			// Determine the frame rate based on the latest sample only
+			Sample delta_time = GetLatestDeltaTime();
+			return (delta_time > 0) ? (1 / delta_time) : 0;
+		}
+
+		Sample GetAverageDeltaTime() const
+		{
+			// If we're still collecting the first 'm_sample.size()', divide by that
+			// Otherwise divide by the number of samples in the ring buffer
+			if (m_samples >= m_sample.size())
+			{
+				ASSERT(m_sum > 0);
+				return (float)m_sum / (float)m_sample.size();
+			}
+			else if (m_samples > 0)
+			{
+				ASSERT(m_sum > 0);
+				return (float)m_sum / (float)m_samples;
+			}
+			return 0;
+		}
+
+		Sample GetAverageFrameRate() const
+		{
+			// Determine the average frame rate based on all samples
+			Sample delta_time = GetAverageDeltaTime();
+			return (delta_time > 0) ? (1 / delta_time) : 0;
+		}
+
+		Sample* GetSamples()
+		{
+			return &m_sample[0];
+		}
+
+		Sample* GetFPS()
+		{
+			return &m_fps[0];
+		}
+
+		int GetSampleSize()
+		{
+			return (int)m_sample.size();
+		}
+
+		int GetSampleOffset()
+		{
+			return (int)m_samples;
+		}
+
+	private:
+		std::array<Sample, SAMPLES> m_sample;
+		std::array<Sample, SAMPLES> m_fps;
+		size_t m_samples;
+		Sample m_sum;
+	};
+
+
+
+	typedef CFrameRateT<float, 60> CFrameRate;
 }

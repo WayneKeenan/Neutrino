253e8b945dad5277a9ed43a531789220a12d8eb4
Gareth Noyce <garethnoyce@triple-eh.net>
2016-12-26 18:20:01 +0200

    Added ForceKill method to the GameState base class

--- source/framework/GameState.h
+++ source/framework/GameState.h
@@ -13,6 +13,13 @@ public:
 	virtual void Update() = 0;
 	virtual void Kill() = 0;
 
+	// Editor modes won't wait for the Enter/In/Exit pattern to complete, instead entry to an editor mode
+	// will cause the framework to call this function for an immediate clean-up and exit 
+	//
+	// If true, the framework will enter the new editor mode, if false it'll ignore the attempt and 
+	// the existing gamestate continues ticking.  
+	virtual bool ForceKill() {return false;}
+
 protected:
 	const char* m_pStateName;
 };

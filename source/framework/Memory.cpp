433dfbf09c73a09f7241902cf4bb17d7568650a5
Gareth Noyce <garethnoyce@triple-eh.net>
2016-12-31 15:06:56 +0200

    Removed the placement allocators for Win32 as I've not worked out the deallocator syntax for VS2015

--- source/framework/Memory.cpp
+++ source/framework/Memory.cpp
@@ -1,81 +1,81 @@
-#include "Memory.h"
-
-#if defined(WIN32)
-
-void *operator new(size_t _size)
-{
-	return ::_aligned_malloc(_size, ALIGN_DEFAULT);
-}
-
-void *operator new[](size_t _size)
-{
-	return ::_aligned_malloc(_size, ALIGN_DEFAULT);
-}
-
-void operator delete(void* _mem)
-{
-	::_aligned_free(_mem);
-}
-
-void operator delete[](void* _mem)
-{
-	::_aligned_free(_mem);
-}
-
-void *operator new(size_t _size, size_t _align)
-{
-	return ::_aligned_malloc(_size, _align);
-}
-
-void *operator new[](size_t _size, size_t _align)
-{
-	return ::_aligned_malloc(_size, _align);
-}
-
-void operator delete(void* _mem, size_t _align)
-{
-	::_aligned_free(_mem);
-}
-
-void operator delete[](void* _mem, size_t _align)
-{
-	::_aligned_free(_mem);
-}
-
-#endif
-
-#if defined(__linux__)
-
-void *operator new(size_t _size)
-{
-	return ::aligned_alloc(ALIGN_DEFAULT, _size);
-}
-
-void *operator new[](size_t _size)
-{
-	return ::aligned_alloc(ALIGN_DEFAULT, _size);
-}
-
-void operator delete(void* _mem)
-{
-	::free(_mem);
-}
-
-void operator delete[](void* _mem)
-{
-	::free(_mem);
-}
-
-void *operator new(size_t _size, size_t _align)
-{
-	return ::aligned_alloc(_align, _size);
-}
-
-void *operator new[](size_t _size, size_t _align)
-{
-	return ::aligned_alloc(_align, _size);
-}
-
-
-
-#endif
+// #include "Memory.h"
+// 
+// #if defined(WIN32)
+// 
+// void *operator new(size_t _size)
+// {
+// 	return ::_aligned_malloc(_size, ALIGN_DEFAULT);
+// }
+// 
+// void *operator new[](size_t _size)
+// {
+// 	return ::_aligned_malloc(_size, ALIGN_DEFAULT);
+// }
+// 
+// void operator delete(void* _mem)
+// {
+// 	::_aligned_free(_mem);
+// }
+// 
+// void operator delete[](void* _mem)
+// {
+// 	::_aligned_free(_mem);
+// }
+// 
+// void *operator new(size_t _size, size_t _align)
+// {
+// 	return ::_aligned_malloc(_size, _align);
+// }
+// 
+// void *operator new[](size_t _size, size_t _align)
+// {
+// 	return ::_aligned_malloc(_size, _align);
+// }
+// 
+// void operator delete(void* _mem, size_t _align)
+// {
+// 	::_aligned_free(_mem);
+// }
+// 
+// void operator delete[](void* _mem, size_t _align)
+// {
+// 	::_aligned_free(_mem);
+// }
+// 
+// #endif
+// 
+// #if defined(__linux__)
+// 
+// void *operator new(size_t _size)
+// {
+// 	return ::aligned_alloc(ALIGN_DEFAULT, _size);
+// }
+// 
+// void *operator new[](size_t _size)
+// {
+// 	return ::aligned_alloc(ALIGN_DEFAULT, _size);
+// }
+// 
+// void operator delete(void* _mem)
+// {
+// 	::free(_mem);
+// }
+// 
+// void operator delete[](void* _mem)
+// {
+// 	::free(_mem);
+// }
+// 
+// void *operator new(size_t _size, size_t _align)
+// {
+// 	return ::aligned_alloc(_align, _size);
+// }
+// 
+// void *operator new[](size_t _size, size_t _align)
+// {
+// 	return ::aligned_alloc(_align, _size);
+// }
+// 
+// 
+// 
+// #endif

7635d6c1a146b5b2a8c8f09987961f421200c1c5
Gareth Noyce <garethnoyce@triple-eh.net>
2016-12-31 16:23:38 +0200

    Batch script to make the resources pack-file on windows

---
+++ build/MakePackfile_Win32.bat
@@ -0,0 +1,7 @@
+del NeutrinoData.tdi 
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\GameConfig.txt
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\background_maps\*
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\shaders\*
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\textures\*
+move NeutrinoData.tdi .\Debug\NeutrinoData.tdi
+PAUSE

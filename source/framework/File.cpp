a16322afbc93997f795668307ca55d370b51eb8d
Gareth Noyce <garethnoyce@triple-eh.net>
2017-03-09 16:43:58 +0200

    Added tilemap Load and Save functions

--- source/framework/File.cpp
+++ source/framework/File.cpp
@@ -4,6 +4,7 @@
 #include "Memory.h"
 #include "Assert.h"
 #include "physfs.h"
+#include "tinydir/tinydir.h"
 #include <string.h>
 
 namespace Neutrino {
@@ -26,9 +27,9 @@ namespace Neutrino {
 	{
 		PHYSFS_init(NULL);
 		PHYSFS_mount( pFilename, NULL, 1);
-	
+
 		const char* err = PHYSFS_getLastError();
-	
+
 		if ( err != NULL )
 		{
 			LOG_ERROR("Data file mount error: %s", err );		
@@ -44,7 +45,7 @@ namespace Neutrino {
 	{
 		PHYSFS_removeFromSearchPath( pFilename );
 		const char* err = PHYSFS_getLastError();
-	
+
 		if ( err != NULL )
 		{
 			LOG_ERROR("Unable to unmount: %s", err );		
@@ -122,8 +123,80 @@ namespace Neutrino {
 	}
 
 
-	bool SaveTileMapData(const TileMapData_t*)
+	bool SaveTileMapData(const TileMapData_t* pData)
 	{
+		// Check filepath is sane
+		tinydir_dir dir;
+		if(tinydir_open(&dir, pData->_sFilepathBuf) == -1)
+		{
+			LOG_ERROR("Output directory is invalid!");
+			return false;
+		}		
+
+		// Create the full path
+		char* sFile = (char*) malloc (strlen(pData->_sFilepathBuf)+strlen(pData->_sFilenameBuf)+6); // plus six as we add a trailing slash and file extension
+		strcpy(sFile, pData->_sFilepathBuf);
+	#if defined _WIN32
+	 strcat(sFile, "\\");
+	#else
+		strcat(sFile, "/");
+	#endif
+		strcat(sFile, pData->_sFilenameBuf);
+		strcat(sFile, ".ltdi" );
+
+		// Check if file already exists
+		if( FileExists(sFile)) LOG_WARNING("%s already exists, overwriting.", sFile);
+
+		TileMapData_t Data = *pData;
+		int iFilenameLength = (int)strlen(pData->_sTextureFilename);
+
+		FILE* pFile = fopen(sFile, "w+");
+		if(NULL != pFile)
+		{
+			fwrite(&Data._fVersion, sizeof(float), 1, pFile);
+			fwrite(&iFilenameLength, sizeof(int), 1, pFile);
+			fwrite(&Data._sTextureFilename, sizeof(char), strlen(Data._sTextureFilename), pFile);
+			fwrite(&Data._LevelWidth, sizeof(uint16), 1, pFile);
+			fwrite(&Data._LevelHeight, sizeof(uint16), 1, pFile);
+			fwrite(&Data._aTileMap[0], sizeof(int16),  Data._LevelWidth * Data._LevelHeight, pFile);
+		}
+		fclose(pFile);
+
+		// TODO: use LoadTileMapData() to verify the save...
 		return true;
+	}
+
+	const TileMapData_t* LoadTileMapData(const char* sFilePathAndName, const bool bFromResourceBundle = false)
+	{
+		// TODO: Check the length of the file. There should be a minimum filesize that'll indicate a broken file...'
+		FILE* pFile = fopen(sFilePathAndName, "r");
+		if(NULL == pFile)
+		{
+			LOG_ERROR("LoadTileMapData: unable to open %s", sFilePathAndName);
+			return NULL;
+		}
+
+		TileMapData_t* pDataCheck = NEWX(TileMapData_t);
+		int iFilenameLength = 0;
+
+		if(bFromResourceBundle)
+		{
+
+		}
+		else
+		{
+			// TODO: check version against that in the header so we know we don't have a conflict'
+			fread(&(pDataCheck->_fVersion), sizeof(float), 1, pFile);
+			fread(&iFilenameLength, sizeof(int), 1, pFile);
+			pDataCheck->_sTextureFilename = NEWX char[iFilenameLength];
+			fread(&(pDataCheck->_sTextureFilename), sizeof(char), iFilenameLength, pFile);
+			fread(&(pDataCheck->_LevelWidth), sizeof(uint16), 1, pFile);
+			fread(&(pDataCheck->_LevelHeight), sizeof(uint16), 1, pFile);
+			pDataCheck->_aTileMap = NEWX int16[pDataCheck->_LevelWidth * pDataCheck->_LevelHeight];
+			fread(&(pDataCheck->_aTileMap[0]), sizeof(int16), pDataCheck->_LevelWidth * pDataCheck->_LevelHeight, pFile);
+			fclose(pFile);
+		}
+
+		return pDataCheck;
 	}
 }

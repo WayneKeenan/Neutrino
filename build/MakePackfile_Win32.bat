27d8f475f6bfca5615b44d7398f6433536bf3fde
Gareth Noyce <garethnoyce@triple-eh.com>
2017-04-17 11:22:47 +0300

    Added the tilemaps folder to the build script

--- build/MakePackfile_Win32.bat
+++ build/MakePackfile_Win32.bat
@@ -3,5 +3,6 @@ del NeutrinoData.tdi
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\background_maps\*
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\shaders\*
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\textures\*
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\tilemaps\*
 move NeutrinoData.tdi .\Debug\NeutrinoData.tdi
 PAUSE

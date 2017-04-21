f88546f8d1606b1b0c4952627ce86244b8d0505e
Gareth Noyce <garethnoyce@triple-eh.com>
2017-04-21 10:42:51 +0300

    Added internal dimensions for Pixels and FBO render target.

--- source/game/global/CGameGlobals.cpp
+++ source/game/global/CGameGlobals.cpp
@@ -2,9 +2,21 @@
 #include "../../framework/Log.h"
 
 
-void CGameGlobals::SetGameDefaults()
+void CGameGlobals::SetGameDefaults(const NeutrinoPreferences_t* pPreferences)
 {
+	// The framework will pass in the NeutrinoPreferences structure during CoreInit
+	// further calls to SetGamedefaults won't have access to this pointer, and the 
+	// defaults will be set anyway. 
+	if(NULL != pPreferences)
+	{
+		m_iInternalWidth = pPreferences->_iInternalWidth;
+		m_iInternalHeight = pPreferences->_iInternalHeight;
+		m_fPixelWidth = pPreferences->_InternalPixelWidth;
+		m_fPixelHeight = pPreferences->_InternalPixelHeight;
+	}
+
 	m_iLevelNumber = 0;
 	m_iPlayer1Score = 0;
+
 	LOG_INFO("Game Defaults have been set.");
 }
--- source/game/global/CGameGlobals.h
+++ source/game/global/CGameGlobals.h
@@ -1,16 +1,38 @@
 #pragma once
 #include "../../framework/Log.h"
 #include "../../framework/Singleton.h"
+#include "../../framework/Framework.h"
+
+
 
 class CGameGlobals : public CSingleton<CGameGlobals>
 {
-	public:
-		uint8 m_iLevelNumber = 0;
-		uint32 m_iPlayer1Score = 0;
+public:
+	CGameGlobals() { LOG_INFO("Game Globals created"); };
+	~CGameGlobals() { LOG_INFO("Game Globals destroyed"); };
 
-		CGameGlobals() {LOG_INFO("Game Globals created");};
-		~CGameGlobals() {LOG_INFO("Game Globals destroyed");};
 
-		void SetGameDefaults();
+	// Game specific public members
+	//
+	uint8 m_iLevelNumber = 0;
+	uint32 m_iPlayer1Score = 0;
+
+	// Getters
+	//
+	inline float GetPixelWidth() const { return m_fPixelWidth; };
+	inline float GetPixelHeight() const { return m_fPixelHeight; };
+	inline uint16 GetInternalWidth() const { return m_iInternalWidth; };
+	inline uint16 GetInternalHeight() const { return m_iInternalHeight; };
+
+	// Setters
+	//
+	void SetGameDefaults(const NeutrinoPreferences_t* pPreferences);
+
 private:
+	// Internal dimensions, used for rendering to the low res texture and clamping movements
+	//
+	uint16 m_iInternalWidth = 0;
+	uint16 m_iInternalHeight = 0;
+	float m_fPixelWidth = 0.0f;
+	float m_fPixelHeight = 0.0f;
 };

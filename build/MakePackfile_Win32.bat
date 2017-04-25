fe60f90bc59fabdf2d8f72d74e4cc3168ae32099
Gareth Noyce <garethnoyce@triple-eh.com>
2017-04-25 21:36:48 +0300

    Added the post process settings to the windows build script

--- build/MakePackfile_Win32.bat
+++ build/MakePackfile_Win32.bat
@@ -1,5 +1,6 @@
 del NeutrinoData.tdi 
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\GameConfig.txt
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\NeutrinoPost.tdi
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\background_maps\*
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\shaders\*
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\textures\*

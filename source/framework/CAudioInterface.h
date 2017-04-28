c1ba8ca121237406f35c137144ef1c14b6e4b8a9
Gareth Noyce <garethnoyce@triple-eh.com>
2017-04-28 16:25:54 +0300

    Audio Interface outline

--- source/framework/CAudioInterface.h
+++ source/framework/CAudioInterface.h
@@ -5,12 +5,20 @@ namespace Neutrino {
 		
 		public:
 			virtual void SetGlobalVolume(const int iVolume) =0;
-			
+
 			virtual void PauseAll()=0;
 			virtual void ResumeAll()=0;
 
-			virtual int LoadSample(const char* pFilepathAndName) =0;
-			virtual bool PlaySample(const int iSampleID) =0;
+			virtual void PauseMusic()=0;
+			virtual void ResumeMusic()0=;
+
+			virtual int LoadSample(const char* pFilepathAndName)=0;
+			virtual bool PlaySample(const int iSampleID)=0;
 		
+			virtual void FadeOutMusic()=0;
+			virtual void FadeInMusic()=0;
+
+			virtual void DeallocateAllSamples()=0;
+			virtual void DeallocateAllMusic()=0;
 	};
 };

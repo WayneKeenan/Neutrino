24eea709aef137c77e9d7ad383de7c666b3437a8
Gareth Noyce <garethnoyce@triple-eh.com>
2017-05-03 20:48:10 +0300

    Made the colour functions inline

--- source/framework/Colour.cpp
+++ source/framework/Colour.cpp
@@ -3,21 +3,6 @@
 
 namespace Neutrino
 {
-	uint32 GetPackedColour(const float fR, const float fG, const float fB, const float fA)
-	{
-		return (uint32)((uint8)(fA * 255.0f) << 24) | ((uint8)(fB * 255.0f) << 16) | ((uint8)(fG * 255.0f) << 8) | (uint8)(fR * 255.0f);
-	}
-
-	uint32 GetPackedColourV4(const glm::vec4 vColour)
-	{
-		return (uint32)((uint8)(vColour.w * 255.0f) << 24) | ((uint8)(vColour.z * 255.0f) << 16) | ((uint8)(vColour.y * 255.0f) << 8) | (uint8)(vColour.x * 255.0f);
-	}
-
-	uint32 GetPackedColourV4(const glm::vec4* pColour)
-	{
-		return (uint32)((uint8)(pColour->w * 255.0f) << 24) | ((uint8)(pColour->z * 255.0f) << 16) | ((uint8)(pColour->y * 255.0f) << 8) | (uint8)(pColour->x * 255.0f);
-	}
-
 	uint32 GetPackedColourHSV(const float fH, const float fS, const float fV, const float fA)
 	{
 		int i;
--- source/framework/Colour.h
+++ source/framework/Colour.h
@@ -10,15 +10,30 @@ namespace Neutrino
 	// GetPackedColour()
 	// 		Returns colour from RBG (0.0f -> 1.0f range) inputs to AABBGGRR packed int.
 	//   	Suitable for the packed VBO vert definition used within the framework.  
-	uint32 GetPackedColour(const float fR, const float fG, const float fB, const float fA);
+	inline uint32 GetPackedColour(const float fR, const float fG, const float fB, const float fA)
+	{
+		return (uint32)((uint8)(fA * 255.0f) << 24) | ((uint8)(fB * 255.0f) << 16) | ((uint8)(fG * 255.0f) << 8) | (uint8)(fR * 255.0f);
+	};
 
 	// GetPackedColourV4()
 	// 		Returns colour from glm::vec4 input to AABBGGRR packed int.
 	//   	Suitable for the packed VBO vert definition used within the framework.  
-	uint32 GetPackedColourV4(const glm::vec4 vColour);
-	uint32 GetPackedColourV4(const glm::vec4* pColour);
+	inline uint32 GetPackedColourV4(const glm::vec4 vColour)
+	{
+		return (uint32)((uint8)(vColour.w * 255.0f) << 24) | ((uint8)(vColour.z * 255.0f) << 16) | ((uint8)(vColour.y * 255.0f) << 8) | (uint8)(vColour.x * 255.0f);
+	};
+
+	inline uint32 GetPackedColourV4(const glm::vec4* pColour)
+	{
+		return (uint32)((uint8)(pColour->w * 255.0f) << 24) | ((uint8)(pColour->z * 255.0f) << 16) | ((uint8)(pColour->y * 255.0f) << 8) | (uint8)(pColour->x * 255.0f);
+	};
 
 	// GetPackedColourHSV
 	// 		Returns colour from HSV (0.0f -> 1.0f range) inputs to AABBGGRR packed int.
 	uint32 GetPackedColourHSV(const float fH, const float fS, const float fV, const float fA);
 }
+
+
+
+
+

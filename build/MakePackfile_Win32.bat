7f1459a1348f39355f94c4fc3d222b1eba44c4ed
Gareth Noyce <garethnoyce@triple-eh.com>
2017-06-08 19:20:52 +0300

    Fixed up the pack file bat file to compress the bigger items first. 7z can't move its temp files reliably

--- build/MakePackfile_Win32.bat
+++ build/MakePackfile_Win32.bat
@@ -1,13 +1,11 @@
 del NeutrinoData.tdi 
-7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\GameConfig.txt
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\GameConfig.ini
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\audio\music\*
+7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\audio\sfx\*
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\NeutrinoPost.tdi
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\background_maps\*
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\shaders\*
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\textures\*
 7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\tilemaps\*
-7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\audio\music\*
-7z a -tZip NeutrinoData.tdi e:\BitBucket\Neutrino\resources\audio\sfx\*
-
-
 move NeutrinoData.tdi .\Debug\NeutrinoData.tdi
 PAUSE
